7ab89d935cfcf7ade8d21aba72790a48f97a7dcc
Ashutosh Nanda <ashutosh.nanda@gmail.com>
2015-01-09 20:19:11 -0500

    Started analysis file

---
+++ sale_analysis.R
@@ -0,0 +1,13 @@
+#Conditional Probabilities through Bootstrapping -> Thresholding based on clustering (1D)
+#Graph of interconnected things (like that D3 graph) once we pair stuff up
+#Graphical Network Analysis (eigenvalues)
+#PCA!!
+#Clustering analysis?
+
+#Writing this up in an Rmd file
+
+
+
+get_table <- function() {
+    
+}
